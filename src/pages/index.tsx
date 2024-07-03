cba0538d9304f33b146eba27a698cd3111777745
linhf123 <wb-lhf780012@antgroup.com>
2024-07-03 11:33:53 +0800

    fix: closed default upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
-import { InboxOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { message, Upload, Row, Col, Modal, Form, Input, Space, Select, Button } from 'antd';
-import * as xlsx from 'xlsx';
-import dayjs from 'dayjs';
-import { useRequest } from 'ahooks';
+import React, { useState } from "react";
+import { InboxOutlined } from "@ant-design/icons";
+import type { UploadProps } from "antd";
+import {
+  message,
+  Upload,
+  Row,
+  Col,
+  Modal,
+  Form,
+  Input,
+  Space,
+  Select,
+  Button,
+} from "antd";
+import * as xlsx from "xlsx";
+import dayjs from "dayjs";
+import { useRequest } from "ahooks";
 
 const { Dragger } = Upload;
 
@@ -16,10 +27,10 @@ async function handleReadableStreamAsPDF(readableStream, filename) {
   const url = URL.createObjectURL(blob);
 
   // 创建隐藏的可下载链接
-  const downloadLink = document.createElement('a');
+  const downloadLink = document.createElement("a");
   downloadLink.href = url;
   downloadLink.download = `${dayjs().format()}_${filename}`; // 自定义文件名
-  downloadLink.style.display = 'none';
+  downloadLink.style.display = "none";
 
   // 将链接添加到DOM中并触发点击事件以开始下载
   document.body.appendChild(downloadLink);
@@ -30,40 +41,43 @@ async function handleReadableStreamAsPDF(readableStream, filename) {
   URL.revokeObjectURL(url);
 }
 
-
 const App: React.FC = () => {
   const [excelList, setExcelList] = useState([]);
   const [pdf, setPdf] = useState(null);
   const [modal, contextHolder] = Modal.useModal();
 
   const pdfProps: UploadProps = {
-    name: 'file',
+    name: "file",
     maxCount: 1,
     multiple: false,
+    accept: ".pdf",
+    beforeUpload: () => {
+      return false;
+    },
     onChange(info) {
       const { status } = info.file;
-      if (status === 'removed') {
-        setPdf(null)
+      if (status === "removed") {
+        setPdf(null);
       }
-      if (status === 'done') {
-        setPdf(info.file.originFileObj)
+      if (status === "done") {
+        setPdf(info.file.originFileObj);
         message.success(`${info.file.name} file uploaded successfully.`);
-      } else if (status === 'error') {
+      } else if (status === "error") {
         message.error(`${info.file.name} file upload failed.`);
       }
     },
     onDrop(e) {
-      console.log('Dropped files', e.dataTransfer.files);
+      console.log("Dropped files", e.dataTransfer.files);
     },
   };
 
   function processData(data) {
-    const workbook = xlsx.read(data, { type: 'array' });
+    const workbook = xlsx.read(data, { type: "array" });
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
 
     const result = xlsx.utils.sheet_to_json(worksheet);
-    setExcelList(result)
+    setExcelList(result);
   }
 
   function readFile(file) {
@@ -74,98 +88,104 @@ const App: React.FC = () => {
     };
     reader.readAsArrayBuffer(file);
   }
-  const getExcelNameList = () => window.excelNameList || []
-  const options = Object.keys(excelList[0] || {}).map(key => {
+  const getExcelNameList = () => window.excelNameList || [];
+  const options = Object.keys(excelList[0] || {}).map((key) => {
     return {
       label: key,
-      value: key
-    }
-  })
+      value: key,
+    };
+  });
   const [form] = Form.useForm();
 
-
-  const { loading, run } = useRequest((data) => {
-    return fetch('/api/resetPdf', {
-      method: 'POST',
-      body: data
-    })
-  }, {
-    manual: true,
-    onSuccess: res => {
-      console.log(res, 'res')
-      if (res.status === 200) {
-        handleReadableStreamAsPDF(res.body, pdf.name)
-      } else {
-        message.error('上传失败，可以尝试修改表格列名，PDF 暂时不支持中文水印')
-      }
+  const { loading, run } = useRequest(
+    (data) => {
+      return fetch("/api/resetPdf", {
+        method: "POST",
+        body: data,
+      });
+    },
+    {
+      manual: true,
+      onSuccess: (res) => {
+        console.log(res, "res");
+        if (res.status === 200) {
+          handleReadableStreamAsPDF(res.body, pdf.name);
+        } else {
+          message.error(
+            "上传失败，可以尝试修改表格列名，PDF 暂时不支持中文水印"
+          );
+        }
+      },
     }
-  })
-
+  );
 
   return (
-    <Row gutter={24} style={{ marginTop: '20vh', paddingLeft: '20vw' }} >
+    <Row gutter={24} style={{ marginTop: "20vh", paddingLeft: "20vw" }}>
       <Col>
         <Dragger
           name="file"
           multiple={false}
           maxCount={1}
           beforeUpload={(file) => {
-            const restList = getExcelNameList()
-            const isExcel = ['xls', 'xlsx', 'csv', ...restList].some(type => file.name.includes(type))
+            const restList = getExcelNameList();
+            const isExcel = ["xls", "xlsx", "csv", ...restList].some((type) =>
+              file.name.includes(type)
+            );
             if (!isExcel) {
               message.error(`${file.name} is not a excel file`);
             }
-            return isExcel || Upload.LIST_IGNORE;
+            // false 不触发自动请求
+            return isExcel ? false : Upload.LIST_IGNORE;
           }}
-
           onChange={(info) => {
             const { status } = info.file;
 
-            if (status === 'removed') {
-              setExcelList([])
+            if (status === "removed") {
+              setExcelList([]);
             }
-            if (status === 'done') {
-              readFile(info.file.originFileObj)
+            if (status === "done") {
+              readFile(info.file.originFileObj);
               message.success(`${info.file.name} file uploaded successfully.`);
-            } else if (status === 'error') {
+            } else if (status === "error") {
               message.error(`${info.file.name} file upload failed.`);
             }
           }}
-          onDrop={e => {
-            console.log('Dropped files', e.dataTransfer.files);
+          onDrop={(e) => {
+            console.log("Dropped files", e.dataTransfer.files);
           }}
         >
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
           <p className="ant-upload-text">上传 Excel</p>
-          <p className="ant-upload-hint">
-            支持点击和拖拽上传
-          </p>
+          <p className="ant-upload-hint">支持点击和拖拽上传</p>
         </Dragger>
-      </Col >
+      </Col>
       <Col>
         <Dragger {...pdfProps} disabled={excelList.length === 0}>
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
           <p className="ant-upload-text">上传 PDF</p>
-          <p className="ant-upload-hint">
-            支持点击和拖拽上传
-          </p>
+          <p className="ant-upload-hint">支持点击和拖拽上传</p>
         </Dragger>
       </Col>
 
-      {
-        excelList.length > 0 && <Col>
+      {excelList.length > 0 && (
+        <Col>
           <Form form={form} name="validateOnly" autoComplete="off">
-            <Form.Item name="columnName" label="表格列名" initialValue={options[0]?.value} required>
+            <Form.Item
+              name="columnName"
+              label="表格列名"
+              initialValue={options[0]?.value}
+              required
+            >
               <Select options={options} />
             </Form.Item>
-            <Form.Item name="x" label="水印 x 轴" initialValue={20} >
+            <Form.Item name="x" label="水印 x 轴" initialValue={20}>
               <Input />
             </Form.Item>
-            <Form.Item name="y" label="水印 y 轴" initialValue={20} >
+            <Form.Item name="y" label="水印 y 轴" initialValue={20}>
               <Input />
             </Form.Item>
             <Form.Item name="size" label="水印文字大小" initialValue={12}>
@@ -178,25 +198,28 @@ const App: React.FC = () => {
                 disabled={!pdf}
                 onClick={() => {
                   form.validateFields().then(({ size, x, y, columnName }) => {
-                    const formData = new FormData()
-                    const columnList = excelList.map(item => item[columnName])
-                    formData.append('pdf', pdf);
-                    formData.append('size', size);
-                    formData.append('x', x);
-                    formData.append('y', y);
-                    formData.append('list', JSON.stringify(columnList || []));
+                    const formData = new FormData();
+                    const columnList = excelList.map(
+                      (item) => item[columnName]
+                    );
+                    formData.append("pdf", pdf);
+                    formData.append("size", size);
+                    formData.append("x", x);
+                    formData.append("y", y);
+                    formData.append("list", JSON.stringify(columnList || []));
 
-                    run(formData)
-                  })
-                }}>
+                    run(formData);
+                  });
+                }}
+              >
                 PDF 添加水印
               </Button>
             </Form.Item>
           </Form>
         </Col>
-      }
-    </Row >
-  )
+      )}
+    </Row>
+  );
 };
 
 export default App;
